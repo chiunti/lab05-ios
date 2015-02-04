692217a269645087328487bb27fe01f008ad571b
chiunti <usuario@C3.local>
2015-02-04 16:00:18 -0600

    Initial Commit

---
+++ lab05/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  lab05
+//
+//  Created by Chiunti on 04/02/15.
+//  Copyright (c) 2015 chiunti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ lab05/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lab05
+//
+//  Created by Chiunti on 04/02/15.
+//  Copyright (c) 2015 chiunti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
